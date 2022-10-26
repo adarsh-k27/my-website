981fbdb8fca90f84d4a3a3a15639e0d76b90bd70
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-26 15:11:19 +0530

    changed size of project

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -2,18 +2,19 @@ import React, { useContext } from 'react'
 import { Project } from '../components'
 import Portfolio_Context from '../context'
 import { Projects } from '../assets/data'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 function PortfolioPage () {
   const { color_change } = useContext(Portfolio_Context)
   console.log('ppp', Projects)
   return (
-    <motion.div 
-    initial={{ x: 200, opacity: 0 }}
+    <motion.div
+      initial={{ x: 200, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: 0, opacity: 0 }}
       transition={{ delay: 0.3 }}
-    className='w-screen md:w-full  md:h-full h-screen flex flex-col gap-3 '>
-      <div className='mt-11 md:mt-8 ml-3 md:ml-0'>
+      className='w-screen md:w-full  md:h-full h-screen flex flex-col gap-3 '
+    >
+      <div className='mt-14 md:mt-8 ml-3 md:ml-0'>
         <h1
           className={`w-full  text-black/70 dark:text-white/80 text-xl md:text-xl font-[oswald] tracking-wider`}
         >
@@ -22,7 +23,7 @@ function PortfolioPage () {
         <div className={`w-[3rem] h-[2px] bg-${color_change}`}></div>
         <div className={`w-[2rem] h-[1px] bg-${color_change} mt-1`}></div>
       </div>
-      <div className='mt-4 md:mt-6 w-full flex  scroll-smooth overflow-x-scroll gap-2 md:gap-4 items-center justify-start'>
+      <div className='mt-10 md:mt-6 w-full flex  scroll-smooth overflow-x-scroll gap-2 md:gap-4 items-center justify-start'>
         {Projects &&
           Projects.map(project => {
             return <Project data={project} />
