2b0a02fd30bc743838d8274eb596a9b1e94c1d00
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-26 10:53:50 +0530

    add all projects

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,44 +1,64 @@
-import {FcDataConfiguration,FcMultipleDevices,FcDebian} from '../icons'
+import {
+    FcDataConfiguration,
+    FcMultipleDevices,
+    FcDebian
+} from '../icons'
 export const MyServices = [{
-    Title: "Front End Developer",
-    icon:<FcMultipleDevices /> ,
-    Desc: "A front-end developer is someone who works on software, like desktop or mobile websites, that users interact with. Specifically, a front-end developer spends time on the side of technology that the user touches and sees.The front end is also commonly referred to as the client side, or more broadly speaking, the user interface.I design a responsive intreractive meaningful website with Html,Css,Taiwindcss and many tools.iam a strong and effective front-end developer shows a determined focus and obsession on what users and customers need and want."
-},{
-    Title:"Back End Developer",
-    icon:<FcDataConfiguration />,
-    Desc: "Iam  A back-end developer who writes code that forms the backbone of a website.Back-end developers are the experts who build and maintain the mechanisms that process data and perform actions on websites. Unlike front-end developers, who control everything you can see on a website, back-end developers are involved in data storage, security, and other server-side functions that you cannot see.They use code that helps browsers communicate with databases, store, understand, and delete data.I write Reusable code and secure code for your website."
-},
-{
-    Title:"Web Frame Designer",
-    icon:<FcDebian />,
-    Desc: "If you're creating a real web site for a real company, you have to choose a design that's not quite as exciting. Something more responsible and restrained. Something dependable.The design must be Powerful, Attractive and Respomsive.I will help you to create your website design.Today there are many tool for that"
-}]
+        Title: "Front End Developer",
+        icon: < FcMultipleDevices / > ,
+        Desc: "A front-end developer is someone who works on software, like desktop or mobile websites, that users interact with. Specifically, a front-end developer spends time on the side of technology that the user touches and sees.The front end is also commonly referred to as the client side, or more broadly speaking, the user interface.I design a responsive intreractive meaningful website with Html,Css,Taiwindcss and many tools.iam a strong and effective front-end developer shows a determined focus and obsession on what users and customers need and want."
+    }, {
+        Title: "Back End Developer",
+        icon: < FcDataConfiguration / > ,
+        Desc: "Iam  A back-end developer who writes code that forms the backbone of a website.Back-end developers are the experts who build and maintain the mechanisms that process data and perform actions on websites. Unlike front-end developers, who control everything you can see on a website, back-end developers are involved in data storage, security, and other server-side functions that you cannot see.They use code that helps browsers communicate with databases, store, understand, and delete data.I write Reusable code and secure code for your website."
+    },
+    {
+        Title: "Web Frame Designer",
+        icon: < FcDebian / > ,
+        Desc: "If you're creating a real web site for a real company, you have to choose a design that's not quite as exciting. Something more responsible and restrained. Something dependable.The design must be Powerful, Attractive and Respomsive.I will help you to create your website design.Today there are many tool for that"
+    }
+]
 //https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666757432/music-app_bkjncf.jpg
 //https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666642383/chatapp-1_v0tcqs.jpg
-export const Projects=[
-{
-    Title:"Food Delivery Web App",
-    Features:["Redux","TailwindCss","Cloudinary"],
-    Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666642375/food-app_ypzmes.jpg",
-    Desc:"It Is An Food Delivery Web App.We Can Find Food Here and Buy It"
-},
-{
-    Title:"Music Learning Web App ",
-    Features:["FireBase","Context State","Tailwind","React"],
-    Image: "",
-    Desc:"Its An Music Learning Web App.Wecan Hear Music Here And Relax"
-}, {
-    Title: "Happy Talky Chat App",
-    Features: ["Socket.io","React","NodeJs"],
-    Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666642365/chat-app-3_fdfywr.jpg",
-    Desc: "It Is An Happy Catting Application.Chat With Person Who is Already Logged In ,With Typing Indicater"
-}, {
-    Title: "Random Chat App",
-    Features: ["Socket.Io", "React", "css"],
-    Image: "",
-    Desc: "Its An Random Chatting Application.We can select Person in It For Chatting"
-}, 
-{
+export const Projects = [{
+        Title: "Food Delivery Web App",
+        Features: ["Redux", "TailwindCss", "Cloudinary"],
+        Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666642375/food-app_ypzmes.jpg",
+        Desc: "It Is An Food Delivery Web App.We Can Find Food Here and Buy It"
+    },
+    {
+        Title: "Music Learning Web App ",
+        Features: ["FireBase", "Context State", "Tailwind", "React"],
+        Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666757432/music-app_bkjncf.jpg",
+        Desc: "Its An Music Learning Web App.Wecan Hear Music Here And Relax"
+    }, {
+        Title: "Happy Talky Chat App",
+        Features: ["Socket.io", "React", "NodeJs"],
+        Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666642365/chat-app-3_fdfywr.jpg",
+        Desc: "It Is An Happy Catting Application.Chat With Person Who is Already Logged In ,With Typing Indicater"
+    }, {
+        Title: "Random Chat App",
+        Features: ["Socket.Io", "React", "css"],
+        Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666642383/chatapp-1_v0tcqs.jpg",
+        Desc: "Its An Random Chatting Application.We can select Person in It For Chatting"
+    },
+    {
+        Title:"Responsive Company web",
+        Features:["React","Css","Bootstrap"],
+        Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666761391/port-2_pcmbfm.jpg",
+        Desc:"Its a Simple Responsive Website For a Company"
+    },
+    {
+        Title: "Responsive Portfolio",
+        Features: ["React", "Css"],
+        Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666760976/port_nipjzw.jpg",
+        Desc: "It Is A Simple Responsive Portfolio WebSite"
+    },
+    {
+     Title:"Animation Website",
+     Features:["Splice","React","Tailwind"],
+     Image: "https://res.cloudinary.com/doxpbnjnk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1666761657/anim-website_lpvf2x.jpg",
+     Desc:"It Is a Animation Website. when we Hover Mouse On Home Screen Visual.The Animation Will Happen.Done With Splice"
+    },
     
-}
 ]
