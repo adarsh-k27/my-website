b1a7426d3c244afe54693ab1844d0d96f5441bcc
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-26 15:07:59 +0530

    changed portfolio color

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ProjectDesc from './ProjectDesc'
 function Project ({ data }) {
   return (
-    <div className='w-full  md:w-300 h-340 min-w-300 md:min-w-300 flex flex-col gap-2 bg-lightSideBar bg-opacity-40 dark:bg-opacity-100 dark:bg-white/10 py-[.5rem] md:py-[1rem] rounded-md'>
+    <div className='w-full  md:w-300 h-340 min-w-300 md:min-w-300 flex flex-col gap-2 bg-lightSideBar bg-opacity-60 dark:bg-opacity-100 dark:bg-white/10 py-[.5rem] md:py-[1rem] rounded-md'>
       <img
         src={data.Image && data.Image}
         alt=''
