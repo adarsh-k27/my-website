4b15e956490c280b256dded4306cfcf5c2d60e5f
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-26 23:20:50 +0530

    OUTSIDE CLICK

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
         `App ${mode}`
       } 
       onClick={()=>{
+        console.log("jahhjhbayayu");
         if(open){
+          console.log("ooopened");
           setOpen(false)
         }
       }}
