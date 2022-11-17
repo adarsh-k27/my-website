80c70d6ea4f9c7744be69321860639f3317e4048
adarsh-k27 <adarshkdev27@gmail.com>
2022-11-17 10:21:11 +0530

    change all i think

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -18,7 +18,7 @@ function PortfolioPage () {
       animate={{ x: 0, opacity: 1 }}
       exit={{ x: 0, opacity: 0 }}
       transition={{ delay: 0.3 }}
-      className='w-screen md:w-full  md:h-auto h-screen flex flex-col gap-3 '
+      className='w-screen md:w-full  md:h-auto h-screen mb-1 flex flex-col gap-3 overflow-y-scroll'
     >
       <div className='mt-[3.4rem] md:mt-8 ml-3 md:ml-3'>
         <div className='flex flex-row w-[50%] z-50 items-center justify-center'>
@@ -28,8 +28,8 @@ function PortfolioPage () {
             PortFolio
           </h1>
           <ScrollSide setScrollDir={setScrollDir} ScrollDir={ScrollDir} />
-          <div className='ml-6'>
-            <p className='text-white text-[.68rem] tracking-wider'>
+          <div className='ml-6 hidden md:block'>
+            <p className='text-black text-[.68rem] tracking-wider  dark:text-white'>
               email:sample@gmail.com, pass:sample,
               abhinandh0027@gmail.com,1234567{' '}
             </p>
@@ -37,6 +37,11 @@ function PortfolioPage () {
         </div>
         <div className={`w-[3rem] h-[2px] bg-${color_change}`}></div>
         <div className={`w-[2rem] h-[1px] bg-${color_change} mt-1`}></div>
+        <div className='ml-6 md:hidden mt-3 '>
+          <p className='text-black  dark:text-white text-[.68rem] tracking-wider'>
+            email:sample@gmail.com, pass:sample, abhinandh0027@gmail.com,1234567{' '}
+          </p>
+        </div>
       </div>
       <div
         ref={Scroll}
